22ea36b401e376336757f7adb1c1af68280651c8
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2024-04-29 23:03:36 +0700

    add

--- src/views/AsthmaView.vue
+++ src/views/AsthmaView.vue
@@ -102,7 +102,7 @@
           <img
             src="@/assets/asthema.png"
             alt="รูปภาพ"
-            style="width: auto; height: 65vw"
+            style="width: auto; height: 50vw"
           />
           <div>
             <button @click="closePopup" class="btn btn-secondary mt-2">
